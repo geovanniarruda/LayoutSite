32bd87e1e7e61cc0a013f054059aa2060930529a
Geovanni Arruda <geovanniarruda@gmail.com>
2021-10-08 14:25:49 -0300

    js no input de busca e pequenas mudanças

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-
+// CARROSSEL
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -13,22 +13,16 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
   slides[slideIndex -1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
 }
 
 
-
-
+// Menu Hamburguer 
 function mostrarMenu() {
   var x = document.getElementById("navegacao");
   if (x.style.display === "flex") {
@@ -38,13 +32,21 @@ function mostrarMenu() {
   }
 }
 
+// Correção de um bug ao ampliar a tela apos ocultar a navegaçao
 function exibirNav() {
   let largura = window.innerWidth;
 
   if (largura > 768) {
     document.getElementById("navegacao").style.display = "flex";
     
-  }
+  } 
+}
 
-  
+
+// JavaScript da busca
+function teste() {
+let busca = document.getElementById("busca");
+let pesquisado = busca.value;
+
+alert(`A busca foi por: ${pesquisado}`)
 }
